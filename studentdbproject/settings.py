677734b98ee4b15e65ee830217d6865ff1f09d97
Peng Wah Ng <pengwah@yahoo.com>
2018-06-27 18:39:50 -0700

    ver1.0

--- studentdbproject/settings.py
+++ studentdbproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'students.apps.StudentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'studentdbproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'studentsdb',
+        'USER': 'postgres',
+        'PASSWORD': 'genius',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
