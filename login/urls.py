be0da7a571d10d34076fa5ddb09cda96cac20539
Peng Wah Ng <pengwah@yahoo.com>
2018-06-28 00:23:18 -0700

    ver1.2

--- login/urls.py
+++ login/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name='login')
-    path('logout', views.logout, name='logout')
+    path('', views.login, name='login'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 ]
