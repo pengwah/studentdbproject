b6242a9426db0d6c9a2f09c2c8bb1352840c99a4
Peng Wah Ng <pengwah@yahoo.com>
2018-06-27 23:47:54 -0700

    ver1.1

--- students/models.py
+++ students/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+import datetime
 
 class student(models.Model):
-    firstname = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
+    firstname = models.CharField(max_length=40)
+    middlename = models.CharField(max_length=40, null=True)
+    lastname = models.CharField(max_length=40)
+    birthdate = models.DateField(null=True)
+    email = models.EmailField(null=True)
