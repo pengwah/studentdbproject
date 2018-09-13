8799f043ca32f44216f9db4f7405feb79e9fa590
Peng Wah Ng <pengwah@yahoo.com>
2018-09-12 23:20:58 -0700

    ver2.0

--- studentdbproject/settings.py
+++ studentdbproject/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'studentdbproject/static/')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 
